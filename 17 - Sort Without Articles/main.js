30b29f9d9568cef5efa6f1056df548057fe3306d
Glooory <glooorypu@gmail.com>
2018-04-27 16:24:16 +0800

    Add 17 - Sort Without Articles

---
+++ 17 - Sort Without Articles/main.js
@@ -0,0 +1,13 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bandsList = document.querySelector('#bands');
+
+function strip(bandName) {
+    return bandName.replace(/^(a |an |the )/i, '').trim();
+}
+
+bandsList.innerHTML = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1)
+    .map(band => {
+        return `
+            <li>${band}</li>
+        `;
+    }).join('');
